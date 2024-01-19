8de3cb8c190094134b0894164329df121232e4b2
Vladislav <Ledovskikh.vladislav@yandex.ru>
2024-01-19 12:36:39 +0300

    Итог

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿void Multiplication (int a, int b) // Функция, выполняющая действие разности
+﻿void Multiplication (int a, int b) // Функция, выполняющая действие умножения
 {
     int mult = a * b;
     Console.WriteLine("Произведение чисел равно: " + mult);
