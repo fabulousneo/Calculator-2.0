fc325c0ced6c731ef43b3b198c36c36cbfcad485
Vladislav <Ledovskikh.vladislav@yandex.ru>
2024-01-19 13:26:08 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,28 +1,5 @@
-Console.WriteLine("Калькулятор");
-
-Console.WriteLine("Введите число A:");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число B:");
-int b = int.Parse(Console.ReadLine());
-
-Console.Write("Выбери действие: 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление");
-string choise = Console.ReadLine();
-
-switch (choise) {
-    case "1": Console.WriteLine(sum(a, b));
-    break;
-    
-    case "2": Console.WriteLine(difference(a, b));
-    break;
-    
-    case "3": Console.WriteLine(multiplication(a, b));
-    break;
-    
-    case "4": Console.WriteLine(division(a, b));
-    break;
-    
-    default:
-        Console.WriteLine("Неверные данные");
-        break;
+void sum (int a, int b) // Функция, которая выполняет действие сложения
+{
+   int summ = a + b;
+   Console.WriteLine("Сумма чисел равна: " + summ);
 }
-    
