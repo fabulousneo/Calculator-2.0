791072000a40d5536047e7c51a183820bd6baed1
Apillene <ikuplinov14@gmail.com>
2024-01-19 12:00:18 +0300

    создание функции выполнения сложения

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Проверка"); 
+﻿void sum (int a, int b) // Функция, которая выполняет действие сложения
+{
+   int summ = a + b;
+   Console.WritrLine("Сумма чисел равна: " + summ);
+}
