4d120bdfca9e8d2f118d4ffbe7af12ac22641b8b
Vladislav <Ledovskikh.vladislav@yandex.ru>
2024-01-19 11:31:30 +0300

    Start

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Проверка"); 
+Console.WriteLine("Калькулятор");
+
+Console.Write("Выбери действие: 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление");
+string choise = Console.ReadLine();
+
